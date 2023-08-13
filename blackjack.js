4209dafaa5a1c51e5eece74a2714e1c3216fd31a
santiagoc1106 <133804703+santiagoc1106@users.noreply.github.com>
2023-08-13 16:07:04 -0400

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -101,3 +101,11 @@ function checkAce(card){
  }
   return 0;
 }
+
+function reduceAce(playerSum, playerAceCount) {
+  while(playerSum> 21 && playerAceCount> 0) {
+    playerSum -= 10;
+    playerAceCount -= 1;
+  }
+  return playerSum;
+}
